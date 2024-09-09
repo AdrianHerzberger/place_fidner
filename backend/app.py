5ea76f03d937bf1b2c8bd74211f952cd8207814a
Adrian Herzberger <adrianherzberger94@gmail.com>
2024-09-09 14:59:00 +0200

    project setup

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+app_key = os.getenv("app_key")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002, debug=True)
